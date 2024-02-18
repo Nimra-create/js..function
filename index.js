0b71f003537eb1fede2023a0406223df25723e7b
Nimra-create <nimraraza985@gmail.com>
2024-02-18 09:44:46 -0800

    message

---
+++ index.js
@@ -0,0 +1,67 @@
+
+
+
+
+function calculateTotalCost(price, quantity) {
+    return price * quantity;
+}
+
+// Call the function and store the result in a variable
+let totalCost = calculateTotalCost(10, 5);
+
+// Print the result using console.log()
+console.log("Total cost:", totalCost);
+
+
+
+// //question 2
+// function calculateArea(length, width) {
+//     return length * width;
+// }
+
+// // Call the function and store the result in a variable
+// let totalarea = calculateArea(10, 5);
+
+// // Print the result using console.log()
+// console.log("Total area:", totalarea);
+
+
+//question 2
+// function calculateBmi(length, width) {
+//     return length * width;
+// }
+
+// // Call the function and store the result in a variable
+// let totalarea = calculateArea(10, 5);
+
+// // Print the result using console.log()
+// console.log("Total area:", totalarea);
+
+//QUESTION 3
+// function calculateBMI(weight, height) {
+//     return weight / (height * height);
+  
+// }
+
+//  let totalbmi = calculateBMI(10, 5);
+//  console.log("Total BMI:", totalbmi);
+
+
+//question 4
+function TriangleArea(base, height) {
+        return (base * height);
+      
+    }
+    
+     let totalarea= TriangleArea(10, 5);
+     console.log("Total area :", totalarea);
+    
+    
+
+
+
+
+
+
+
+
